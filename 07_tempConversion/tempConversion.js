9685d887931fb873ac3f1f43f440f30fae3f5266
moulanbator <arnaudtmoulin@gmail.com>
2024-08-30 17:50:11 +0200

    Exo 7 completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(year) {
+  farenheitToCels = Math.round(((year - 32) * (5/9)) * 10) / 10;
+  return farenheitToCels;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(year) {
+  celcsiusToFar = Math.round((year * (9/5) + 32) * 10) / 10;
+  return celcsiusToFar;
 };
 
 // Do not edit below this line
@@ -9,3 +13,6 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+console.log(convertToCelsius(32));
+console.log(convertToFahrenheit(0));
