e72d27de4f3a8df0a0c589f680d7dd0e1aa8581e
moulanbator <arnaudtmoulin@gmail.com>
2024-08-30 15:38:28 +0200

    Exo 5 completed, easier than 4 for sure

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+let final = 0;
+if (min > max) {
+    [min, max] = [max, min]
+}
+// console.log("min: " + min + " max: " + max)
+if (min < 0 || max < 0 || Number.isInteger(min) == false || 
+    Number.isInteger(max) == false || typeof min !== 'number' ||
+    typeof max !== 'number'){
+    return "ERROR";
+}
+for (min; min <= max; min++) {
+final += min; 
+}
+return final;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+console.log(sumAll(2, 3.2));
