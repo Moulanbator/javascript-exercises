3803de498d17a3430e15af41e6b5af0b2b685a29
moulanbator <arnaudtmoulin@gmail.com>
2024-08-30 13:20:11 +0200

    Exo 2 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,26 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+let output = ""
+if (times < 0) {return "ERROR"};
+for (let i = 0; i < times; i++){
+    output = output + word;
+}
+    return output;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+// console.log(repeatString("he",39));
+
+// const repeatString = function (word, times) {
+//     if (times < 0) return "ERROR";
+//     let string = "";
+//     for (let i = 0; i < times; i++) {
+//       string += word;
+//     }
+//     return string;
+//   };
+  
+//   module.exports = repeatString;
+
+//   console.log(repeatString("hey", 7));
