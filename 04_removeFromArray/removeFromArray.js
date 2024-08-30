129e4ffa7a62bfe5128bb23c22fa431ab249ffe9
moulanbator <arnaudtmoulin@gmail.com>
2024-08-30 15:02:21 +0200

    Exo 4 completed with more difficulty

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(input, ...removals) {
+    removals.forEach(remove => {
+        let index;
+        while ((index = input.indexOf(remove)) !== -1){
+            input.splice(index, 1);
+        }
+    })
+    // let index = input.indexOf(remove);
+    // if (index !== -1){
+    //     input.splice(index, 1)
+    // }
+    return input;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// console.log(removeFromArray([1, 2, 2, 3, 4], 1, 2 ));
