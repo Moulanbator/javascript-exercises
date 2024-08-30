60b881b8d1f95787269fab858bdecc5cc53d8c54
moulanbator <arnaudtmoulin@gmail.com>
2024-08-30 13:39:41 +0200

    Exo 3 completed with more ease

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+let splitWord = word.split("");
+let reverseLetters = splitWord.reverse();
+let joinLetters = reverseLetters.join("");
+return joinLetters;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+console.log(reverseString("football"));
