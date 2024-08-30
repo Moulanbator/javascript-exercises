c0dae719901b39aa9599ebc42bf7c8b8ca9f6f7d
moulanbator <arnaudtmoulin@gmail.com>
2024-08-30 17:26:48 +0200

    Exo6 completed, this was a brain tickler

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
-};
-
+const leapYears = function(year) {
+if (year % 4 == 0){
+    if (year % 100 == 0){
+       if (year % 400 == 0){
+            return true; //leaper2 
+       } else return false; //not2
+    } else return true; //leaper1
+} else return false; //not1
+}
 // Do not edit below this line
 module.exports = leapYears;
+
+console.log(leapYears(2000))
